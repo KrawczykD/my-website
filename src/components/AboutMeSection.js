20403c31aa5dda0d8dc0c37bb051c148f1b27df9
Damian Krawczyk <damiankrawczyk23@gmail.com>
2019-03-12 12:40:12 +0000

    about me section start create

--- src/components/AboutMeSection.js
+++ src/components/AboutMeSection.js
@@ -4,7 +4,34 @@ import './AboutMeSection.css';
 const AboutMeSection=()=>{
     return(
         <div id="2" className="AboutMeSection">
-            AboutMeSection
+            <h1>ABOUT ME</h1>
+            <p className="infoAboutMe">My name is Damian Krawczyk and I am junior web developer from Manchester</p>
+            <p>I create this web page as a my portfolio to expose myself. Please check this whole website to get more information about me , my work and knowledge.</p>
+            <div className="interestings">
+                <p>HOBBIES & INTERESTS</p>
+                <i class="fas fa-laptop-code">
+                    <p>Coding</p>
+                </i>
+                <i class="fas fa-headphones-alt">
+                    <p>Music</p>
+                </i>
+                <i class="fas fa-car">
+                    <p>Cars</p>
+                </i>
+                <i class="fas fa-snowboarding">
+                    <p>Mountainboarding</p>
+                </i>
+
+                <i class="fas fa-wrench">
+                    <p>Mechanics</p>
+                </i>
+                <i class="far fa-lightbulb">
+                    <p>New Technologies</p>
+                </i>
+                <i class="fas fa-utensils">
+                    <p>Cooking</p>
+                </i>
+            </div>
         </div>
     )
 }
